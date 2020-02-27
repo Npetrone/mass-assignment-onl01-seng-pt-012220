e466bda4a80866f2c665f2572f3b4cb38c159087
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-27 00:06:30 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,4 @@
 class Person
-  attr_accesor :name, :birthday, :hair_color, :eye_color, :height, 
-  
+  attr_accesor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, 
   
 end
